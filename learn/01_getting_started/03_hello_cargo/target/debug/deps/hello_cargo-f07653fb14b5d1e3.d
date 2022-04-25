2f49829b7dc1f33055d49adb71b03e1f73a69572
Skye-LAB <60174369+Skye-LAB@users.noreply.github.com>
2022-04-25 16:34:38 +0700

    change: menghapus folder target

--- learn/01_getting_started/03_hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3.d
+++
@@ -1,5 +0,0 @@
-/home/skye/Documents/MyDesk/Rust/learning_rust/learn/01_getting_started/03_hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3: src/main.rs
-
-/home/skye/Documents/MyDesk/Rust/learning_rust/learn/01_getting_started/03_hello_cargo/target/debug/deps/hello_cargo-f07653fb14b5d1e3.d: src/main.rs
-
-src/main.rs:
--- learn/01_getting_started/03_hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/skye/Documents/MyDesk/Rust/learning_rust/learn/01_getting_started/03_hello_cargo/target/debug/hello_cargo: /home/skye/Documents/MyDesk/Rust/learning_rust/learn/01_getting_started/03_hello_cargo/src/main.rs
