958fc6b47cbd4db526b6cede2843b59868768d7a
Skye-LAB <walksky452@gmail.com>
2022-04-29 19:07:41 +0700

    change: menambahkan bagian looping di program control_flow

--- learn/03_common_programming_concepts/05_control_flow/src/main.rs
+++ learn/03_common_programming_concepts/05_control_flow/src/main.rs
@@ -14,7 +14,8 @@ fn main() {
     let condition = true;
     let number = if condition { 5 } else { 6 };
 
-    // looping loop
+    // looping
+    // loop
     let mut counter = 0;
 
     let result = loop {
@@ -26,4 +27,20 @@ fn main() {
     };
 
     println!("The result is {}", result);
+
+    // while
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    // for
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
 }
