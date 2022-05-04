5e9d5f8de210a05738c9279573f8c606db28d518
Skye-LAB <walksky452@gmail.com>
2022-05-04 16:10:48 +0700

    change: menambahkan kode pada reference_and_borrowing

--- learn/04_understanding_ownership/02_references_and_borrowing/src/main.rs
+++ learn/04_understanding_ownership/02_references_and_borrowing/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    // mutable reference
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    let r1 = &mut s;
+    // error, karena tidap boleh ada dua mutable reference
+    // let r2 = &mut s;
+    //
+    // println!("{}, {}", r1, r2);
+}
+
+// fungsi ini mengambil sebuah reference ke String
+// sebagai parameter
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
